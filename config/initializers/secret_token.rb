b3bc94a7492d12ac1870debb03de7ba5916dcb94
Andre Joseph Cubeta <ajcubeta@gmail.com>
2011-09-22 23:39:57 +0800

    Push framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'a5f69608b5e7ea106ed2903c948116e20664dedec31277fe433b2bafc62e465a4ebd8d12218dd615ac8f3fcb44ee2e55ee1b784a6e8e064624ad266c92e42900'
